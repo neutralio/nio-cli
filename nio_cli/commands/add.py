934018449d4339c050effeebf6b6fa43357c4545
cowleyk <cowley.kevin.l@gmail.com>
2017-07-26 09:44:18 -0600

    cli renames appropriate files

--- nio_cli/commands/add.py
+++ nio_cli/commands/add.py
@@ -28,6 +28,8 @@ class Add(Base):
         for block in self._blocks:
             if self._upgrade:
                 self._upgrade_block(block)
+            self._rename_files(block)
+            #TODO: SHOULD THIS BE IN DIFFERENT FOR LOOP?
         # Install block dependencies
         for block in self._blocks:
             block_path = 'blocks/{}'.format(block)
@@ -42,3 +44,22 @@ class Add(Base):
         subprocess.call(checkout.format('blocks', block, 'master'), shell=True)
         update = "cd ./{}/{} && git pull --ff-only --recurse-submodules --all"
         subprocess.call(update.format('blocks', block), shell=True)
+
+    def _rename_files(self, block):
+        rename_block_file = "cd ./{}/{} && mv example_block.py {}_block.py"
+        # It's correct to assume they are inside a n.io project directory?
+        subprocess.call(rename_block_file.format("blocks", block, block))
+        rename_test_file = \
+            "cd ./{}/{} && mv test_example_block.py test_{}_block.py"
+        subprocess.call(rename_test_file.format("blocks", block, block))
+        rename_readme = "cd ./{}/{} && mv BLOCK_README.md README.md"
+        subprocess.call(rename_readme.format("blocks", block))
+
+    # TODO:
+        # `git remote remove origin`
+        # `git add -A`
+        # git commit --amend --reset-author -m "Initial commit"
+        # `git remote add origin`
+            # HOW will this be done?  Prompt to enter a github repo url?
+        # `git push --set-upstream origin master`
+            # Do we want to do this? Auto-push to master?
